2dfb1d6ad977c4e14e4186cf20e479b6a2d8e7da
leosencia <jfquinones@alumni.up.edu.ph>
2026-03-23 02:12:53 +0800

    comment out asset precompilation and cleaning commands in render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,8 +2,8 @@
 set -o errexit
 
 bundle install
-bin/rails assets:precompile
-bin/rails assets:clean
+# bin/rails assets:precompile
+# bin/rails assets:clean
 
 # bundle exec rails db:migrate
 # bundle exec rails db:seed
