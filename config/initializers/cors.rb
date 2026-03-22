f0fdcce76307e4f0f39af71c342e17d6e0a61fc1
leosencia <jfquinones@alumni.up.edu.ph>
2026-03-23 01:49:21 +0800

    chore: update CORS configuration to allow requests from production frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173"
+    origins "http://localhost:5173",
+            "https://support-ticket-frontend-e0sj.onrender.com"
 
     resource "*",
       headers: :any,
